eaf95b7f005d38a0447197aedc8de6b7c57de6af
Mats Sjöberg <mats.sjoberg@csc.fi>
2023-07-05 09:35:59 +0300

    Added nvidia-smi logging example

---
+++ run-ddp-gpu4-smi-logging.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --account=project_2001659
+#SBATCH --partition=gputest
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=0
+#SBATCH --time=15
+#SBATCH --gres=gpu:v100:4
+
+module purge
+module load pytorch
+
+# Old way with torch.distributed.run
+# srun python3 -m torch.distributed.run --standalone --nnodes=1 --nproc_per_node=4 mnist_ddp.py --epochs=100
+
+nvidia-smi --query-gpu=index,temperature.gpu,utilization.gpu,utilization.memory --format=csv -l > gpu.log &
+BG_PID=$!
+
+# New way with torchrun
+srun torchrun --standalone --nnodes=1 --nproc_per_node=4 mnist_ddp.py --epochs=2
+
+kill $BG_PID
